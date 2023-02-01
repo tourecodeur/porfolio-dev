b7885ba78c983a9db505ebc85155d59280fec137
tourecodeur <116043808+tourecodeur@users.noreply.github.com>
2023-02-01 23:19:57 +0000

    Add files via upload

---
+++ skills/index.js
@@ -0,0 +1,49 @@
+// ["html","css3","javascript","nodejs","php","laravel","wordpress","mysql","mongodb","react","sass","python","django","linux",]
+import html from "./html.svg";
+import css3 from "./css3.svg";
+import javascript from "./javascript.svg";
+import nodejs from "./nodejs.svg";
+import php from "./php.svg";
+import laravel from "./laravel.svg";
+import wordpress from "./wordpress.svg";
+import mysql from "./mysql.svg";
+import mongodb from "./mongodb.svg";
+import react from "./react.svg";
+import sass from "./sass.svg";
+import python from "./python.svg";
+import django from "./django.svg";
+import linux from "./linux.svg";
+
+export const svgs = [
+  "html",
+  "css3",
+  "javascript",
+  "nodejs",
+  "php",
+  "laravel",
+  "wordpress",
+  "mysql",
+  "mongodb",
+  "react",
+  "sass",
+  "python",
+  "django",
+  "linux",
+];
+
+export default {
+  html,
+  css3,
+  javascript,
+  nodejs,
+  php,
+  laravel,
+  wordpress,
+  mysql,
+  mongodb,
+  react,
+  sass,
+  python,
+  django,
+  linux,
+};
